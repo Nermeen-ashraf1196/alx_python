fcdff1e50a1657ba0b0021f80ba06f85030baff2
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-20 18:13:36 +0300

    1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,3 +2,7 @@ word = 'Holberton'
 print ("First 3 letters: {}".format(word[:3]))
 print ("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:8]))
+a = 'School'
+print ("First 3 letters: {}".format(a[:3]))
+print ("Last 2 letters: {}".format(a[-2:]))
+print("Middle word: {}".format(a[1:5]))
