fea1930dc3f5b19587f42bab1cca9c9fdfe6e65f
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-20 21:54:54 +0300

    4

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,12 +1,11 @@
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-print("Last digit of {} is {} and is ".format(number, digit), end="")
-if digit > 5:
-    print("greater than 5")
-elif digit == 0:
-    print("0")
+num_str = repr(number)
+last_digit_str = num_str[-1]
+last_digit = int(last_digit_str)
+if int(last_digit_str) > 5:
+    print (f"Last digit of {number} is {last_digit}","and is greater than 5")
+elif int(last_digit_str) == 0:
+    print(f"Last digit of {number} is {last_digit}","is Zero")
 else:
-    print("less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit}","and is less than 6 and not 0")
--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,5 +1,2 @@
-#for x in range (0,99):
-#    print(x,'=',hex(x))
-txt = "{0} = {0:X}"
-for x in range(0,99):
-    print(txt.format(x))
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
