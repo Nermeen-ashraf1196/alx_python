0c77a7b2e3d0312f07b489f33a9a0eba9843e15e
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 17:54:09 +0300

    5

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -9,6 +9,6 @@ def is_prime(number):
             if (number % i) == 0:
                 return(True)
             else:
-                return(True)
+                return(False)
     else:
         return(False)
