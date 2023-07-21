459b490c8bbfc35a78763c554af18616b3144765
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 17:28:33 +0300

    2

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,3 +1,3 @@
 def pow(a, b):
-    """Return a to the power of b."""
+ # Return a to the power of b.
     return (a ** b)
---
+++ python-functions/2-temperature.py
@@ -0,0 +1,3 @@
+def convert_to_celsius(fahrenheit):
+ # Return a to the celsius degree.
+    return ((5/9) * (a - 32))
