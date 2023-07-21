c9976fdb6ec6bb575d5737c620badfd481518413
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 17:35:45 +0300

    2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,3 @@
 def convert_to_celsius(fahrenheit):
  # Returns the temperature in Celsius.
-    return ((5/9) * (convert_to_celsius(fahrenheit) - 32))
+    return ((5/9) * (fahrenheit - 32))
---
+++ python-functions/3-string.py
@@ -0,0 +1,3 @@
+def reverse_string(string)
+ # Returns the reversed string.
+ 
