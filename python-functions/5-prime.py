f385cf1055e80f476309b07e2a16d1b2b9b3476f
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 17:50:25 +0300

    5

---
+++ python-functions/5-prime.py
@@ -0,0 +1,14 @@
+def is_prime(number):
+ # Returns True if the number is prime, and False otherwise.
+ # If given number is greater than 1
+    if number > 1:
+     # Iterate from 2 to n / 2
+        for i in range(2, int(number/2)+1):
+         # If num is divisible by any number between
+         # 2 and n / 2, it is not prime
+            if (number % i) == 0:
+                return(False)
+            else:
+                return(True)
+    else:
+        return(False)
