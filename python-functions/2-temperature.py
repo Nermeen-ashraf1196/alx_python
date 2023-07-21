97f841a95df4da9b5a3ebf1858537cd728242f24
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 18:36:19 +0300

    2

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 def convert_to_celsius(fahrenheit):
  # Returns the temperature in Celsius.
     celsius = (5/9) * (fahrenheit - 32)
-    round(celsius,2)
-    return celsius
+    rounded_number = round(celsius, 2)  
+    return rounded_number
   
