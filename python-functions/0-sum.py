9bb6b71673c01cb6935041fedc166e08ebb0c9c0
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 15:51:38 +0300

    0

---
+++ python-functions/0-sum.py
@@ -0,0 +1,12 @@
+# Define a list of numbers
+numbers = [10, 20, 30, 40, 50]
+ 
+# Initialize a variable to store the sum
+total = 0
+ 
+# Iterate through the list and add each number to the total
+for num in numbers:
+    total += num
+ 
+# Print the sum of the numbers
+print(total)
---
+++ python-functions/README.md
@@ -0,0 +1,3 @@
+# Python - Functions
+<p> Programming is like building a multilingual puzzle</p>
+<img src="images/readme2.jpg" alt="python" title="Python"> 
