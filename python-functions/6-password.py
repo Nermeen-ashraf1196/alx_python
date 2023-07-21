4a169703bfd899aaa08d7325e0d6c83537ee5f65
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 18:25:53 +0300

    6

---
+++ python-functions/6-password.py
@@ -0,0 +1,17 @@
+def validate_password(password):
+ # Returns True if the password passes all the checks, and False otherwise.
+    length = len(password)
+    check_uppercase=0
+    check_lowercase=0
+    if length < 8:
+        return False
+    for num in range(length):
+        if password[num].isupper():
+            check_uppercase+=1
+        if password[num].islower():
+            check_lowercase+=1
+        if password[num] == " ":
+            return False
+    if check_uppercase == 0 or check_lowercase == 0:
+        return False
+    return True
