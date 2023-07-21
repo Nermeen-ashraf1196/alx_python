c9e4e79a5ee2494b93cbd7fa926ed57cb61d4511
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-21 17:14:44 +0300

    0

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,12 +1,5 @@
-# Define a list of numbers
-numbers = [10, 20, 30, 40, 50]
- 
-# Initialize a variable to store the sum
-total = 0
- 
-# Iterate through the list and add each number to the total
-for num in numbers:
-    total += num
- 
-# Print the sum of the numbers
-print(total)
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
