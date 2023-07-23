a7870d362d6a7d9f5980e45edfca0098ab29a7f8
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-07-23 20:20:52 +0300

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
-def add(a, b):
-    # My addition function
-    return (a + b)
+if __name__ == "__main__":
+ #Print the sum of 1 and 2.
+    from add_0 import add
 
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
