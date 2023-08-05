bf9603928ffdac239ba1056df7c4eddeaad9a3d8
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-05 19:48:24 +0300

    2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,8 +1,14 @@
-"""checks inheritance""" 
+"""A defining function."""
 def inherits_from(obj, a_class):
-    """ Returns true/false if it's inherited or not """
-    if obj == [1, 2, 3] and a_class == list:
-        return False
-    if obj == 1 and a_class == int:
-        return False
-    return isinstance(obj, a_class)
+    """Check if the object is an instance of a class
+    that inherited from the given class.
+    Args:
+        obj (object): The object to be checked.
+        a_class (class): The specified class to check inheritance from.
+    Returns:
+        bool: True if the object is an instance of 
+        a class inherited from 'a_class', otherwise False."""
+    classes = type(obj).mro()
+    # Check if 'a_class' is in the set of classes the object belongs to
+    return any(a_class is c for c in classes[1:]) 
+     # Start from index 1 to skip the object's own class
