a82ef40f8ed31a8719b598ed4f502c03c063eb14
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-05 18:13:14 +0300

    1

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,15 +1,14 @@
-def safe_print_division(a, b):
+def safe_print_division(a,b):
     try:
+        
         result = a / b
+ 
+    except ValueError:
+        print("Invalid input. Please enter valid integers.")
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
-        return None
-    except TypeError:
-        print("Error: Both inputs must be integers.")
-        return None
+        result = None
     except Exception as e:
         print("An unexpected error occurred:", e)
-        return None
     finally:
         print("Inside result: {}".format(result))
         return result
