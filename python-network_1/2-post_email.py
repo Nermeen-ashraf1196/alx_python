94f4153c946dbb06eb5ea946c17f48e154458ebc
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-10 20:03:06 +0300

    y

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,43 @@
-"""importing requests"""
+"""
+   A script that Sends a POST request to the given URL with the provided email as a parameter.
+   
+   Returns:
+        str: The response body of the POST request.
+"""
+
+import sys
 import requests
 
-"""importing sys"""
-import sys
+def send_post_request(url, email):
+    """
+    Sends a POST request to the given URL with the provided email as a parameter.
 
-url = sys.argv[1]
-email = sys.argv[2]
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to be sent as a parameter.
 
-request = requests.post(url , data = {'email':email} )
-response = request.json()
-print("Your email is:", response.get('email'))
+    Returns:
+        str: The response body of the POST request.
+    """
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    return response.text
+
+def main():
+    """
+    Main function that processes command line arguments and sends the POST request.
+    """
+    if len(sys.argv) != 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    print("Email:", email)
+    
+    response_body = send_post_request(url, email)
+    
+
+if __name__ == "__main__":
+    main()
