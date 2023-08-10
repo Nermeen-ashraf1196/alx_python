57c8dd2d5d2e99b4d43747ead13364606c4b3718
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-10 20:52:19 +0300

    yb

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,17 +1,24 @@
-"""Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
-"""
-import sys
-import requests
+""" HTTP Request Script"""
 
+import requests
+import sys
+
+def make_request(url):
+    try:
+        response = requests.get(url)
+        print(response.status_code)
+
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+    finally: 
+        pass
+    
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    url = sys.argv[1]
+    make_request(url)
         
