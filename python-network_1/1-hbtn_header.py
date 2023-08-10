f11a9c076621473347fd2c86024c4526c840928d
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-10 20:01:34 +0300

    y

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,29 @@
-"""importing requests"""
+"""
+Sends a request to a URL and displays the value of the variable X-Request-Id in the response header.
+Uses the requests and sys packages only.
+"""
+
+import sys
 import requests
 
-"""importing sys"""
-import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    
+    try:
+        response = requests.get(url)
+        x_request_id = response.headers.get('X-Request-Id')
+        
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("None")
+            
+    except requests.RequestException as e:
+        print("An error occurred: {}".format(e))
 
-url = sys.argv[1]
-
-
-request = requests.get(url)
-print(request.headers['X-Request-Id'])
+if __name__ == "__main__":
+    main()
