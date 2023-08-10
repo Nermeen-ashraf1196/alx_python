69df91bd00dc9b0811d6c2bdaab8524effd25b87
Nermeen-ashraf1196 <nermin.ashraf11@gmail.com>
2023-08-10 20:05:54 +0300

    y

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,42 +1,16 @@
-"""
-    Main function that takes a URL as input, sends a request to the URL,
-    and displays the body of the response. If the HTTP status code is
-    greater than or equal to 400, it prints an error message with the
-    status code.
-
-    Usage: python 4-error_code.py <URL>
-"""
-
+"""importing requests"""
 import requests
+
+"""importing sys"""
 import sys
 
-def main():
-    """
-    Main function that takes a URL as input, sends a request to the URL,
-    and displays the body of the response. If the HTTP status code is
-    greater than or equal to 400, it prints an error message with the
-    status code.
+url = sys.argv[1]
+#request = requests.get("https://alu-intranet.hbtn.io/status")
 
-    Usage: python 4-error_code.py <URL>
-    """
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python 4-error_code.py <URL>")
-        sys.exit(1)
-    
-    url = sys.argv[1]  # Get the URL from command-line arguments
+request = requests.get(url)
+status = request.status_code
 
-    try:
-        response = requests.get(url)  # Send a GET request to the URL
-
-        if response.status_code >= 400:
-            # If the status code is 400 or higher, print an error message
-            print(f"Error code: {response.status_code}")
-        else:
-            # If the status code is less than 400, print the response body
-            print(response.text)
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-
-if __name__ == "__main__":
-    main()
+if ( status >= 400):
+    print("Error code:",status)
+else:
+    print("Regular request")
---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,42 @@
+"""
+This script takes a letter as input and sends a POST request to http://0.0.0.0:5000/search_user.
+The letter is sent as the parameter 'q'.
+If no argument is given, 'q' is set to an empty string.
+If the response body is properly JSON formatted and not empty, it displays the id and name in the format: [<id>] <name>.
+Otherwise, it displays appropriate error messages.
+Only the 'requests' and 'sys' packages are used.
+"""
+
+import requests
+import sys
+
+def main():
+    """ Sends a POST request to http://0.0.0.0:5000/search_user and processes the response
+    """
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    response = requests.post(url, data=data)
+    try:
+        response_json = response.json()
+        
+        if response_json:
+            user_id = response_json.get("id")
+            user_name = response_json.get("name")
+            
+            if user_id is not None and user_name is not None:
+                print(f"[{user_id}] {user_name}")
+            else:
+                print("Not a valid JSON")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    main()
